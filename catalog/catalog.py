1f2b1f0d5cdffe20372d73af1188e3d5f085f09b
Corey Gray <csgray@alaska.edu>
2016-04-08 13:29:28 -0800

    Add basic Jinja2 templates.

--- catalog/catalog.py
+++ catalog/catalog.py
@@ -1,15 +1,48 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import Flask
+from flask import Flask, render_template, url_for
+from database_setup import Character, Deity, Guild, Race, Skill
+
 app = Flask(__name__)
 
 engine = create_engine('sqlite:///characters.db')
-Session = sessionmaker(bind=engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home/')
 def home():
-    return 'This is a test!'
+    return render_template('home.html')
+
+
+@app.route('/characters/')
+def characters():
+    c = session.query(Character).all()
+    return render_template('characters.html', characters=c)
+
+
+@app.route('/deities/')
+def deities():
+    d = session.query(Deity).all()
+    return render_template('deities.html', deities=d)
+
+
+@app.route('/guilds/')
+def guilds():
+    g = session.query(Guild).all()
+    return render_template('guilds.html', guilds=g)
+
+
+@app.route('/races/')
+def races():
+    r = session.query(Race).all()
+    return render_template('races.html', races=r)
+
+
+@app.route('/skills/')
+def skills():
+    s = session.query(Skill).all()
+    return render_template('skills.html', skills=s)
 
 if __name__ == '__main__':
     app.debug = True
